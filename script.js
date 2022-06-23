e19a8b01f5004bcf03af905c1e639a4949f861d4
Santhos2904 <santhos2904@gmail.com>
2022-06-22 20:44:46 -0400

    Arrow =>

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const todos = ['Get groceries', 'Clean bike', 'Make dinner'];
-todos.push('another todo');
 
-todos.forEach(function (todoTitle) {
-	let element = document.createElement('div');
-	element.innerText = todoTitle;
-	document.body.appendChild(element);
-});
+render();
+
+addTodo = () => {
+	const textbox = document.getElementById('text-box');
+	const input = textbox.value;
+	todos.push(input);
+
+	render();
+};
+
+function render() {
+	document.getElementById('todo-list').innerHTML = '';
+	todos.forEach((todoTitle) => {
+		const element = document.createElement('div');
+		element.innerText = todoTitle;
+		const todoList = document.getElementById('todo-list');
+		todoList.appendChild(element);
+	});
+}
 
 //coding L1F3
