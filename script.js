44454bc87c5d13fb18ce9f5870a819973c3dc4d7
Santhos2904 <santhos2904@gmail.com>
2022-06-23 22:05:28 -0400

    update

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
-const todos = ['Get groceries', 'Clean bike', 'Make dinner'];
+const todos = [
+	{
+		title: 'Get groceries',
+		dueDate: '2022-06-22',
+	},
+	{
+		title: 'Clean bike',
+		dueDate: '2022-06-23',
+	},
+	{
+		title: 'Make dinner',
+		dueDate: '2022-06-24',
+	},
+];
 
 render();
 
 addTodo = () => {
 	const textbox = document.getElementById('text-box');
-	const input = textbox.value;
-	todos.push(input);
+	const title = textbox.value;
+
+	const datePicker = document.getElementById('date-picker');
+	const dueDate = datePicker.value;
+	todos.push({
+		title: title,
+		dueDate: dueDate,
+	});
 
 	render();
 };
 
 function render() {
 	document.getElementById('todo-list').innerHTML = '';
-	todos.forEach((todoTitle) => {
+	todos.forEach(function (todo) {
 		const element = document.createElement('div');
-		element.innerText = todoTitle;
+		element.innerText = todo.title + ' ' + todo.dueDate;
 		const todoList = document.getElementById('todo-list');
 		todoList.appendChild(element);
 	});
